bfbd82c41802c59e6e76e9a444d9cff60ac36720
Jão Bernardi <joao_bernardi@outlook.com>
2022-03-09 20:05:12 -0300

    Add exercise 2

---
+++ algoritimos/aula_2/exercicio2.py
@@ -0,0 +1,35 @@
+# Input getter
+def get_input(message, set_type, tries=10):
+    while tries >= 0:
+        # Get input
+        inp = input(message)
+        try:
+            # Return the typed input
+            return set_type(inp)
+        except ValueError:
+            # Remove 1 try and continue
+            tries -= 1
+    # Return none if there was no successful input.
+    return None
+
+
+# Define the main function
+def main():
+    # Get the height
+    salAtual = get_input("[?] Escreva o seu salário: ", float)
+    # Don't bother getting the reajuste if the salary is invalid.
+    salVendas = get_input("[?] Escreva o total de vendas: ", float) if salAtual else None
+    # Check if both values are valid
+    if salAtual and salVendas:
+        salFinal = salAtual
+        if salVendas > 100000:
+            salFinal = (5 * (salVendas/100)) + salAtual
+        print(f"[!] Salário final é de R$ {salFinal}")
+    else:
+        print("[!] Salário ou Reajuste inválidos.")
+
+
+# Prevent running main if this is imported.
+if __name__ == "__main__":
+    # Run the code
+    main()
